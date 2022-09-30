4d3d3a2dfdb7b5594958b9ec7d9837b27e9f239a
daniel.khalil <dkses.hef@uea.edu.br>
2022-09-30 14:45:53 -0400

    Inserindo SingleChildScrowView no container

--- lib/Home.dart
+++ lib/Home.dart
@@ -16,72 +16,75 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Álcool ou Gaslina"),
+        title: Text("Álcool ou Gasolina"),
         backgroundColor: Colors.blue
       ),
       body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 32),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
 
-              child: Image.asset("images/logo.png"),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Text(
-                  "Saiba qual a melhor opção para abastecimento do seu carro",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold
-                ),
+                child: Image.asset("images/logo.png"),
               ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Preço Alcool, ex: 1.59"
-              ),
-              style: TextStyle(
-                fontSize: 22,
-              ),
-              controller: _controllerAlcool,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Preço Gasolina, ex: 3.59"
-              ),
-              style: TextStyle(
-                fontSize: 22,
-              ),
-              controller: _controllerGasolina,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: RaisedButton(
-                color: Colors.blue,
-                textColor: Colors.white,
-                padding: EdgeInsets.all(15),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
                 child: Text(
-                    "Calcular",
+                  "Saiba qual a melhor opção para abastecimento do seu carro",
                   style: TextStyle(
-                    fontSize: 20
-                  )
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                  ),
                 ),
-                onPressed: () {},
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top:20),
-              child: Text(
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Alcool, ex: 1.59"
+                ),
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+                controller: _controllerAlcool,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Gasolina, ex: 3.59"
+                ),
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+                controller: _controllerGasolina,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: RaisedButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(15),
+                  child: Text(
+                      "Calcular",
+                      style: TextStyle(
+                          fontSize: 20
+                      )
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top:20),
+                child: Text(
                   "Resultado",
-                textAlign: TextAlign.center,
-              ),
-            )
-          ],
-        ),
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          ),
+        )
       ),
     );
   }
